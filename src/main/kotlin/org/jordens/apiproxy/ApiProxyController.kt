dae174fa39f790b4f7ff56686dd4f77e1bdb35c7
Adam Jordens <ajordens@netflix.com>
2017-07-08 01:06:44 -0700

    A proof of concept declarative api proxy
    Proxy definitions are configurable in `application.yml` as follows:
    
    ```
    proxies:
      - id: flickr
        uri: https://api.flickr.com
        skipHostnameVerification: false
        methods:
          - GET
    ```
    
    Each configured proxy is exposed as `/proxies/{id}/{proxyPath}`.
    
    ex)
    `curl http://localhost:8080/proxies/flickr/services/feeds/photos_public.gne`
    
    This results in a proxied GET request being made to:
    `https://api.flickr.com/services/feeds/photos_public.gne`

---
+++ src/main/kotlin/org/jordens/apiproxy/ApiProxyController.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 Adam Jordens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.jordens.apiproxy
+
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.HandlerMapping
+import java.net.URI
+import java.net.URL
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+@RequestMapping(value = "/proxies")
+class ApiProxyController(val apiProxyConfigurationProperties: ApiProxyConfigurationProperties) {
+    @GetMapping(value = "/{proxy}/**")
+    fun get(@PathVariable(value = "proxy") proxy: String,
+            httpServletRequest: HttpServletRequest) : GenericResponse {
+
+        val proxyConfig = apiProxyConfigurationProperties
+                .proxies
+                .find { it.id.equals(proxy, true) } ?: throw NotFoundException("No proxy config found with id '$proxy'")
+
+        val proxyPath = httpServletRequest
+                .getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE)
+                .toString()
+                .substringAfter("/proxies/$proxy/")
+
+        val okHttpClient = OkHttpClient()
+        val proxiedUrl = proxyConfig.uri + "/" + proxyPath
+        val response = okHttpClient.newCall(Request.Builder().url(proxiedUrl).build()).execute()
+
+        return GenericResponse.ok(mapOf(
+                Pair("proxiedUrl", proxiedUrl),
+                Pair("response", response.body().string()),
+                Pair("responseContentType", response.header("Content-type"))
+        ))
+    }
+}
+
+data class GenericResponse(val status: String,
+                           val code: Int,
+                           val messages: List<String>,
+                           val result: Map<String, Any>) {
+
+    companion object Factory {
+        fun ok(result: Map<String, Any>) = GenericResponse("ok", 200, emptyList(), result)
+        fun ok(result: Pair<String, Any>) = GenericResponse("ok", 200, emptyList(), mapOf(result))
+    }
+}
+
+class NotFoundException(msg: String? = null, cause: Throwable? = null): Exception(msg, cause)
+
+@ControllerAdvice
+class ExceptionHandlers {
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(ex: Throwable, response: HttpServletResponse) {
+        response.sendError(HttpStatus.NOT_FOUND.value(), ex.message)
+    }
+}
